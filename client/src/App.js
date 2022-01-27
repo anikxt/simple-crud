01e2124ac68fd3056edf058c20ae75d52fe762ef
Aniket Kanere <aniket15304@gmail.com>
2022-01-27 19:50:45 +0530

    final build

---
+++ client/src/App.js
@@ -0,0 +1,152 @@
+import React, { useState } from 'react';
+import './App.css';
+import axios from './axios.config';
+
+function App() {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState(0);
+  const [country, setCountry] = useState('');
+  const [position, setPosition] = useState('');
+  const [wage, setWage] = useState(0);
+
+  const [employeeList, setEmployeeList] = useState([]);
+  const [newWage, setNewWage] = useState(0);
+
+  const addEmployee = async () => {
+    try {
+      await axios.post('/create', {
+        name: name,
+        age: age,
+        country: country,
+        position: position,
+        wage: wage,
+      });
+
+      setEmployeeList([
+        ...employeeList,
+        {
+          name: name,
+          age: age,
+          country: country,
+          position: position,
+          wage: wage,
+        },
+      ]);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const getEmployees = () => {
+    axios.get('/employees').then((response) => {
+      setEmployeeList(response.data);
+    });
+  };
+
+  const updateEmployeeWage = async (id) => {
+    try {
+      await axios.put('/update', {
+        wage: newWage,
+        id: id,
+      });
+
+      setEmployeeList(
+        employeeList.map((val) => {
+          return val.id === id
+            ? {
+                id: val.id,
+                name: val.name,
+                country: val.country,
+                age: val.age,
+                position: val.position,
+                wage: newWage,
+              }
+            : val;
+        })
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const deleteEmployee = (id) => {
+    axios.delete(`/delete/${id}`).then((response) => {
+      setEmployeeList(
+        employeeList.filter((val) => {
+          return val.id !== id;
+        })
+      );
+    });
+  };
+
+  return (
+    <div className="App">
+      <div className="information">
+        <label htmlFor="">Name: </label>
+        <input type="text" onChange={(event) => setName(event.target.value)} />
+        <label htmlFor="">Age: </label>
+        <input type="number" onChange={(event) => setAge(event.target.value)} />
+        <label htmlFor="">Country: </label>
+        <input
+          type="text"
+          onChange={(event) => setCountry(event.target.value)}
+        />
+        <label htmlFor="">Position: </label>
+        <input
+          type="text"
+          onChange={(event) => setPosition(event.target.value)}
+        />
+        <label htmlFor="">Wage (year): </label>
+        <input
+          type="number"
+          onChange={(event) => setWage(event.target.value)}
+        />
+        <button onClick={addEmployee}>Add Employee</button>
+        <div className="employees">
+          -----------------------------------------------------------------------------------------
+          <button onClick={getEmployees}>Show Employees</button>
+          {employeeList.map((val, key) => {
+            return (
+              <div className="employee">
+                <div>
+                  <h3>Name: {val.name}</h3>
+                  <h3>Age: {val.age}</h3>
+                  <h3>Country: {val.country}</h3>
+                  <h3>Position: {val.position}</h3>
+                  <h3>Wage: {val.wage}</h3>
+                </div>
+                <div>
+                  <div>Update Wage Here!</div>
+                  <input
+                    type="text"
+                    placeholder="2000..."
+                    onChange={(event) => {
+                      setNewWage(event.target.value);
+                    }}
+                  />
+                  <button
+                    onClick={() => {
+                      updateEmployeeWage(val.id);
+                    }}
+                  >
+                    {' '}
+                    Update
+                  </button>
+                  <button
+                    onClick={() => {
+                      deleteEmployee(val.id);
+                    }}
+                  >
+                    Delete it!
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,77 @@
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const express = require('express');
+const mysql = require('mysql2');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json()); // support json encoded bodies
+
+const db = mysql.createConnection(process.env.MYSQL_URL);
+
+app.post('/api/create', (req, res) => {
+  console.log(req.body);
+  let { name, age, country, position, wage } = req.body;
+
+  db.query(
+    'INSERT INTO employees (name, age, country, position, wage) VALUES (?,?,?,?,?)',
+    [name, age, country, position, wage],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send('Values Inserted');
+      }
+    }
+  );
+});
+
+app.get('/api/employees', (req, res) => {
+  db.query('SELECT * FROM employees', (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.put('/api/update', (req, res) => {
+  const id = req.body.id;
+  const wage = req.body.wage;
+  db.query(
+    'UPDATE employees SET wage = ? WHERE id = ?',
+    [wage, id],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+
+app.delete('/api/delete/:id', (req, res) => {
+  const id = req.params.id;
+  db.query('DELETE FROM employees WHERE id = ?', id, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.use('/static', express.static('public/static'));
+app.all('*', (req, res) =>
+  res.sendFile('public/index.html', { root: __dirname })
+);
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+  console.log(`Yay, your server is running on port ${PORT}`);
+});
