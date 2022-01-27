1b6d3fb5b724f8a5a496aae20a5d69bd19080be5
Aniket Kanere <aniket15304@gmail.com>
2022-01-27 19:56:43 +0530

    fix env var

--- client/src/axios.config.js
+++ client/src/axios.config.js
@@ -1,7 +1,7 @@
 import Axios from 'axios';
 
 const axios = Axios.create({
-  baseURL: 'http://localhost:8080/',
+  baseURL: process.env.RAILWAY_STATIC_URL || 'http://localhost:8080/',
 });
 
 export default axios;
